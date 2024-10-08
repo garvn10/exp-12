130475c3ea7ae76b9dba5130d37a7d93449cb6dd
Garv Nandwana <149328353+garvn10@users.noreply.github.com>
2024-10-08 22:33:19 +0530

    Add files via upload

---
+++ 12A.cpp
@@ -0,0 +1,42 @@
+// NAME - SHIVENDRA DWIVEDI
+// PRN- 23070123122
+// EXPERIMENT - 12(A) 
+
+#include<iostream> 
+#include<string>
+using namespace std; 
+class Data {
+    string name;
+    int roll_no;
+    char dept[50];
+    char division;
+
+    public:
+    Data() {
+        cout<<"Name: ";
+        cin>>name;
+        cout<<"Roll Number: ";
+        cin>>roll_no;
+        cout<<"Department: ";
+        cin>>dept;
+        cout<<"Division: ";
+        cin>>division;
+    }
+    void display() {
+        cout<<"\n"<<"DETAILS:"<<"\n"<<name<<"  "<<roll_no<<"  "<<dept<<"  "<<division<<"\n";
+    }
+};
+int main() 
+{
+    Data d1;
+    d1.display();
+} 
+
+// Output  
+// Name: Shivendra
+// Roll Number: 122
+// Department: E&TC
+// Division: B
+// 
+// DETAILS:
+// Shivendra  122  E&TC  B 
